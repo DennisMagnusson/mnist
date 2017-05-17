b36fa7cbf976661b30e235cc6660fd6d1886435a
d <dennis.magnusson98@gmail.com>
2017-05-17 21:46:00 +0200

    .

--- mnist.py
+++ mnist.py
@@ -9,6 +9,16 @@ batch_size = 5500
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True, reshape=False, validation_size=1)
 
+def convert(l, k):
+	li = []
+	for i in l:
+		if i[k] == 1:
+			li.append([1, 0])
+		else:
+			li.append([0, 1])
+
+	return li
+
 class Net:
 	def __init__(self):
 		sd = 0.1
@@ -27,13 +37,13 @@ class Net:
 			W2 = tf.Variable(tf.truncated_normal([5, 5, c1, c2], stddev=sd))
 			W3 = tf.Variable(tf.truncated_normal([4, 4, c2, c3], stddev=sd))
 			W4 = tf.Variable(tf.truncated_normal([7*7*c3, 200],  stddev=sd))
-			W5 = tf.Variable(tf.truncated_normal([200, 10], stddev=sd))#Change to 200, 2 in future
+			W5 = tf.Variable(tf.truncated_normal([200, 2], stddev=sd))#Change to 200, 2 in future
 
 			B1 = tf.Variable(tf.constant(0.1, tf.float32, [24]))
 			B2 = tf.Variable(tf.constant(0.1, tf.float32, [48]))
 			B3 = tf.Variable(tf.constant(0.1, tf.float32, [64]))
 			B4 = tf.Variable(tf.constant(0.1, tf.float32, [200]))
-			B5 = tf.Variable(tf.constant(0.1, tf.float32, [10]))
+			B5 = tf.Variable(tf.constant(0.1, tf.float32, [2]))
 		
 		#TODO Batch norm this shit
 		Y1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding='SAME')
@@ -56,34 +66,73 @@ class Net:
 		Y14= tf.matmul(Y13, W5) + B5
 		self.Y = tf.nn.softmax(Y14)
 
+		self.cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.Y, labels=y_)
+		self.train_step = tf.train.AdamOptimizer(lr).minimize(self.cross_entropy)
+		self.error = tf.reduce_mean(tf.abs(y_ - self.Y))
+		self.correct = tf.equal(tf.argmax(self.Y, 1), tf.argmax(y_, 1))
+		self.accuracy = tf.reduce_mean(tf.cast(self.correct, tf.float32))
 
+class HyperNet:
+	def __init__(self):
+		#Idea: Just drop the correct one every once in a while
+		sd = 0.1
+		with tf.device("/cpu:0"):
+			W1 = tf.Variable(tf.truncated_normal([10, 64], stddev=sd))
+			W2 = tf.Variable(tf.truncated_normal([64, 10], stddev=sd))
 
-X = tf.placeholder(tf.float32, [None, 28, 28, 1])
-y_= tf.placeholder(tf.float32, [None, 10])
-lr= tf.placeholder(tf.float32)
+			B1 = tf.Variable(tf.constant(0.1, tf.float32, [64]))
+			B2 = tf.Variable(tf.constant(0.1, tf.float32, [10]))
+
+			Y1 = tf.matmul(W1, Xh) + B1
+			Y2 = tf.nn.dropout(Y8, 0.4)
+			Y3 = tf.matmul(W2, Y2) + B2
+			self.Y = tf.nn.softmax(Y3)
+
+X  = tf.placeholder(tf.float32, [None, 28, 28, 1])
+Xh = tf.placeholder(tf.float32, [None, 10])
+y_ = tf.placeholder(tf.float32, [None, 2])
+yh = tf.placeholder(tf.float32, [None, 10])
+lr = tf.placeholder(tf.float32)
 training = tf.placeholder(tf.bool)
 
-"""
 nets = []
 for i in range(10):
-	nets.append(Net(i))
-"""
+	nets.append(Net())
 
-net = Net()
-
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=net.Y, labels=y_)
-train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
-
-error = tf.reduce_mean(tf.abs(y_ - net.Y))
-correct = tf.equal(tf.argmax(net.Y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+#TODO
+#hnet = HyperNet()
+#h_ce = tf.nn.softmax_cross_entropy_with_logist(logits=hnet.Y, labels=y_)
 
 init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
 
-#Training
+#Parallel training
+for n in range(10):
+	net = nets[n]
+	for ep in range(50):
+		for i in range(int(math.floor(55000/batch_size))):
+			batch_X, batch_Y = mnist.train.next_batch(min(batch_size, 55000-batch_size*i))
+			#Learning rate
+			lr_max = 0.02
+			lr_min = 0.0001
+			decay_rate = 1600
+			learning_rate = lr_min + (lr_max - lr_min) * math.exp(-ep/decay_rate)
+
+			labels = convert(batch_Y, n)
+			#print(labels)
+			
+			#This takes way longer with gpu, but only the first time
+			err = sess.run([net.train_step, net.error, net.Y, net.accuracy], feed_dict={X: batch_X, y_: labels, lr: learning_rate, training: True})[3]
+			print(err)
+
+		print("valid_err: ", sess.run(net.accuracy, feed_dict={X: mnist.test.images, y_: convert(mnist.test.labels, n), training: False}))
+
+
+
+#Save this for the future
+#HyperNet training
 for ep in range(50):
 	for i in range(int(math.floor(55000/batch_size))):
 		batch_X, batch_Y = mnist.train.next_batch(min(batch_size, 55000-batch_size*i))
@@ -92,6 +141,31 @@ for ep in range(50):
 		lr_min = 0.0001
 		decay_rate = 1600
 		learning_rate = lr_min + (lr_max - lr_min) * math.exp(-ep/decay_rate)
+
+		labels = convert(batch_Y, n)
+		
+		#This takes way longer with gpu, but only the first time
+		err = sess.run([train_step, error, nets[n].Y], feed_dict={X: batch_X, y_: batch_Y, lr: learning_rate, training: True})[2]
+		print(i)
+
+
+#TODO Remove this in future
+#Training
+for ep in range(50):
+	for i in range(int(math.floor(55000/batch_size))):
+		batch_X, batch_Y = mnist.train.next_batch(min(batch_size, 55000-batch_size*i))
+		#Generate input
+		inp = []
+		for net in nets:
+			inp.append(sess.run(net.Y, feed_dict={X: batch_X, train: False}))
+		
+		#Learning rate
+		lr_max = 0.02
+		lr_min = 0.0001
+		decay_rate = 1600
+		learning_rate = lr_min + (lr_max - lr_min) * math.exp(-ep/decay_rate)
+
+		
 		"""
 		#Train the nets in parallel or seperately?
 		for n in range(len(nets)):
@@ -109,12 +183,3 @@ for ep in range(50):
 
 
 
-def convert(l, k):
-	li = []
-	for i in l:
-		if i[k] == 1:
-			li.append([1, 0])
-		else:
-			li.append([0, 1])
-
-	return np.array(li)
